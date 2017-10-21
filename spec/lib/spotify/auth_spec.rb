e442cae125b3fd9b573fe85aaf9dc78020db6f56
Bilawal Hameed <bilawal@studenthack.com>
2017-10-21 21:25:40 +0200

    added tests

--- spec/lib/spotify/auth_spec.rb
+++ spec/lib/spotify/auth_spec.rb
@@ -6,4 +6,20 @@ RSpec.describe Spotify::Auth do
   it "inherits from OAuth2::Client" do
     expect(Spotify::Auth < OAuth2::Client).to be_truthy
   end
+
+  context "during initialization" do
+    subject { Spotify::Auth.new("client id", "client secret") }
+
+    it "it sets site with api.spotify.com" do
+      expect(subject.site).to be "https://api.spotify.com"
+    end
+
+    it "it sets authorize_url to accounts.spotify.com/oauth/authorize" do
+      expect(subject.options[:authorize_url]).to be "https://accounts.spotify.com/oauth/authorize"
+    end
+
+    it "it does not set token_url" do
+      expect(subject.options[:token_url]).to be "/oauth/token"
+    end
+  end
 end
