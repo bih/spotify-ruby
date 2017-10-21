38e29e1967b8f194de12db87164bff835f55ba1e
Bilawal Hameed <bilawal@studenthack.com>
2017-10-21 21:15:28 +0200

    updated spotify auth file

--- lib/spotify/auth.rb
+++ lib/spotify/auth.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
+require "oauth2"
+
 module Spotify
   class Auth < OAuth2::Client
+    def new(client_id, client_secret)
+      super(client_id, client_secret, site: "https://api.spotify.com")
+    end
   end
 end
