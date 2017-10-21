95a8a9f1ab995091c2939367e4b3eb5ebe1a080b
Bilawal Hameed <bilawal@studenthack.com>
2017-10-21 21:58:43 +0200

    added full test support

--- lib/spotify/auth.rb
+++ lib/spotify/auth.rb
@@ -4,13 +4,39 @@ require "oauth2"
 
 module Spotify
   class Auth < OAuth2::Client
-    def initialize(client_id, client_secret)
+    def initialize(config)
       opts = {
-        site: "https://api.spotify.com",
+        site:          "https://api.spotify.com",
         authorize_url: "https://accounts.spotify.com/oauth/authorize"
       }
-
-      super(client_id, client_secret, opts)
+      validate_initialized_input(config)
+      @redirect_uri = config[:redirect_uri]
+      super(config[:client_id], config[:client_secret], opts)
     end
+
+    def authorize_url
+      super(redirect_uri: @redirect_uri)
+    end
+
+    private
+
+    attr_accessor :redirect_uri
+
+    OAUTH_I18N = {
+      must_be_hash: "Must be a Hash. Example: Spotify::Auth.new({ client_id: '', ... })",
+      require_attr: "Expecting a '%s' key. You can obtain from https://developer.spotify.com"
+    }.freeze
+
+    def validate_initialized_input(config)
+      raise Errors::AuthClientCredentialsError.new(OAUTH_I18N[:must_be_hash]) unless config.is_a?(Hash)
+
+      %i[client_id client_secret redirect_uri].each do |key|
+        raise Errors::AuthClientCredentialsError.new(OAUTH_I18N[:require_attr] % key) unless config.has_key?(key)
+      end
+    end
+  end
+
+  class Errors
+    class AuthClientCredentialsError < StandardError; end
   end
 end
--- spec/lib/spotify/auth_spec.rb
+++ spec/lib/spotify/auth_spec.rb
@@ -6,20 +6,59 @@ RSpec.describe Spotify::Auth do
   it "inherits from OAuth2::Client" do
     expect(Spotify::Auth < OAuth2::Client).to be_truthy
   end
+end
 
+RSpec.describe Spotify::Auth do
   context "during initialization" do
-    subject { Spotify::Auth.new("client id", "client secret") }
+    subject {
+      Spotify::Auth.new(client_id:     "client id",
+                        client_secret: "client secret",
+                        redirect_uri:  "https://localhost")
+    }
 
-    it "it sets site with api.spotify.com" do
-      expect(subject.site).to be "https://api.spotify.com"
+    it "should set site with api.spotify.com" do
+      expect(subject.site).to eq "https://api.spotify.com"
     end
 
-    it "it sets authorize_url to accounts.spotify.com/oauth/authorize" do
-      expect(subject.options[:authorize_url]).to be "https://accounts.spotify.com/oauth/authorize"
+    it "should set authorize_url to accounts.spotify.com/oauth/authorize" do
+      expect(subject.options[:authorize_url]).to eq "https://accounts.spotify.com/oauth/authorize"
     end
 
-    it "it does not set token_url" do
-      expect(subject.options[:token_url]).to be "/oauth/token"
+    it "should not set token_url" do
+      expect(subject.options[:token_url]).to eq "/oauth/token"
+    end
+  end
+end
+
+RSpec.describe Spotify::Auth do
+  context "bad initialization" do
+    it "should raise error without a client_id" do
+      expect {
+        Spotify::Auth.new(client_secret: "client secret",
+                          redirect_uri:  "https://localhost")
+      }.to raise_error Spotify::Errors::AuthClientCredentialsError
+    end
+
+    it "should raise error without a client_secret" do
+      expect {
+        Spotify::Auth.new(client_id:    "client id",
+                          redirect_uri: "https://localhost")
+      }.to raise_error Spotify::Errors::AuthClientCredentialsError
+    end
+
+    it "should raise error without a redirect_uri" do
+      expect {
+        Spotify::Auth.new(client_id:     "client id",
+                          client_secret: "client secret")
+      }.to raise_error Spotify::Errors::AuthClientCredentialsError
+    end
+  end
+end
+
+RSpec.describe Spotify::Auth do
+  context "contains error handlers" do
+    it "when auth credentials invalid" do
+      expect(Spotify::Errors::AuthClientCredentialsError < StandardError).to be_truthy
     end
   end
 end
