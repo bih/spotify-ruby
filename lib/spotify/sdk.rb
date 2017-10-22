adea462d46a8323ce38960b28684950b34be229a
Bilawal Hameed <bilawal@studenthack.com>
2017-10-22 18:09:34 +0200

    added oauth2_access_token method

--- lib/spotify/sdk.rb
+++ lib/spotify/sdk.rb
@@ -3,12 +3,20 @@
 require "spotify/sdk/initialization"
 
 module Spotify
+  ##
+  # Spotify::SDK contains a variety of commands you can do .
+  #
   class SDK
     def initialize(obj)
       payload = Spotify::SDK::Initialization.detect(obj)
       @access_token  = payload[:access_token]
       @expires_at    = payload[:expires_at]
       @refresh_token = payload[:refresh_token]
+    end
+
+    def oauth2_access_token(auth)
+      OAuth2::AccessToken.new(auth, @access_token, expires_at:    @expires_at,
+                                                   refresh_token: @refresh_token)
     end
 
     attr_reader :access_token, :expires_at, :refresh_token
