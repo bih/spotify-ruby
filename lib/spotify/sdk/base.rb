6073fcb634c293779f497e0ea014ed8553a782e9
Bilawal Hameed <bilawal@studenthack.com>
2017-10-22 19:33:02 +0200

    added comments to base

--- lib/spotify/sdk/base.rb
+++ lib/spotify/sdk/base.rb
@@ -8,6 +8,24 @@ module Spotify
     class Base
       include HTTParty
       base_uri "api.spotify.com"
+
+      ##
+      # Initiate a Spotify SDK Base component.
+      #
+      # @example
+      #   @sdk = Spotify::SDK.new("access_token")
+      #   @auth = Spotify::SDK::Base.new(@sdk)
+      #
+      #   @sdk = Spotify::SDK.new("access_token_here")
+      #   @sdk.to_hash # => { access_token: ..., expires_at: ... }
+      #
+      # @param [Spotify::SDK] sdk An instance of Spotify::SDK as a reference point.
+      #
+      def initialize(sdk)
+        @sdk = sdk
+      end
+
+      attr_reader :sdk
     end
   end
 end
--- spec/lib/spotify/sdk/base_spec.rb
+++ spec/lib/spotify/sdk/base_spec.rb
@@ -3,7 +3,22 @@
 require "spec_helper"
 
 RSpec.describe Spotify::SDK::Base do
+  let(:sdk) { Spotify::SDK.new("access_token") }
+  subject { Spotify::SDK::Base.new(sdk) }
+
   it "inherits from HTTParty" do
     expect(Spotify::SDK::Base < HTTParty).to be_truthy
+  end
+
+  context "good initialization" do
+    it "sets @sdk to initialized value" do
+      expect(subject.sdk).to eq sdk
+    end
+  end
+
+  describe "#sdk=" do
+    it "should not be defined" do
+      expect(subject).not_to respond_to(:sdk=)
+    end
   end
 end
