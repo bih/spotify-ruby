b18a8460cfb29db534e0cbdeeda3893d618125f1
Bilawal Hameed <bilawal@studenthack.com>
2017-10-22 02:40:51 +0200

    added full coverage

--- spec/lib/spotify/auth_spec.rb
+++ spec/lib/spotify/auth_spec.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: true
 
 require "spec_helper"
+require "uri"
+require "cgi"
 
 RSpec.describe Spotify::Auth do
   it "inherits from OAuth2::Client" do
     expect(Spotify::Auth < OAuth2::Client).to be_truthy
+  end
+
+  it "has a SCOPES constant" do
+    expect(Spotify::Auth::SCOPES).to be_present
+    expect(Spotify::Auth::SCOPES).to be_a(Array)
+    expect(Spotify::Auth::SCOPES).not_to be_empty
   end
 
   context "during initialization" do
@@ -24,6 +32,70 @@ RSpec.describe Spotify::Auth do
 
     it "should not set token_url" do
       expect(subject.options[:token_url]).to eq "/oauth/token"
+    end
+
+    context "authorize_url is valid" do
+      let(:params) { CGI.parse(URI.parse(authorize_url).query) }
+      let(:client_id) { params["client_id"][0] }
+      let(:redirect_uri) { params["redirect_uri"][0] }
+      let(:response_type) { params["response_type"][0] }
+      let(:scope) { params["scope"][0].split(" ").map(&:to_sym) }
+
+      context "default params" do
+        let(:authorize_url) { subject.authorize_url }
+
+        it "should include the correct base url" do
+          expect(subject.authorize_url).to start_with "https://accounts.spotify.com/oauth/authorize"
+        end
+
+        it "should contain client_id with correct value" do
+          expect(client_id).to eq "client id"
+        end
+
+        it "should contain redirect_uri with correct value" do
+          expect(redirect_uri).to eq "https://localhost"
+        end
+
+        it "should contain response_type with correct value" do
+          expect(response_type).to eq "code"
+        end
+
+        it "should contain all scopes by default" do
+          expect(scope).to eq Spotify::Auth::SCOPES
+        end
+      end
+
+      context "overriding client_id" do
+        let(:authorize_url) { subject.authorize_url(client_id: "override client id") }
+
+        it "should have new value" do
+          expect(client_id).to eq "override client id"
+        end
+      end
+
+      context "overriding redirect_uri" do
+        let(:authorize_url) { subject.authorize_url(redirect_uri: "http://127.0.0.1") }
+
+        it "should have new value" do
+          expect(redirect_uri).to eq "http://127.0.0.1"
+        end
+      end
+
+      context "overriding response_type" do
+        let(:authorize_url) { subject.authorize_url(response_type: "override response type") }
+
+        it "should have new value" do
+          expect(response_type).to eq "override response type"
+        end
+      end
+
+      context "overriding scope" do
+        let(:authorize_url) { subject.authorize_url(scope: "scope1 scope2") }
+
+        it "should have new value as an array with symbolized values" do
+          expect(scope).to eq [:scope1, :scope2]
+        end
+      end
     end
   end
 
