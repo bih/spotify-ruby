5311f288bf14b6a101c5f6e1267f07ee6b078d28
Bilawal Hameed <bilawal@studenthack.com>
2017-10-22 20:44:46 +0200

    added disable flag

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,8 @@ require "webmock/rspec"
 
 Coveralls.wear!
 
+WebMock.disable_net_connect!(allow_localhost: true)
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
