497ac5f1329f3e9032f6c57ccad4b038788ccdc4
Bilawal Hameed <bilawal@studenthack.com>
2017-10-27 06:01:54 +0200

    added Spotify::SDK::Model

--- lib/spotify/sdk/model.rb
+++ lib/spotify/sdk/model.rb
@@ -10,17 +10,44 @@ module Spotify
       # Initialize a new Model instance.
       #
       # @param [Hash] hash The response payload.
-      # @param [Spotify::SDK] parent The SDK object for context. 
+      # @param [Spotify::SDK] parent The SDK object for context.
       #
       def initialize(payload, parent)
+        @payload = payload
+        @parent = parent
+
+        validate_payload
+        validate_parent
+
         super(payload)
-        @parent = parent if @parent
       end
 
       ##
       # A reference to Spotify::SDK::Connect, so we can also do stuff.
       #
       attr_reader :parent
+
+      private
+
+      def validate_payload
+        raise Spotify::Errors::ModelPayloadExpectedToBeHashError.new unless @payload.instance_of? Hash
+      end
+
+      def validate_parent
+        raise Spotify::Errors::ModelParentInvalidSDKObjectError.new unless @parent.instance_of? Spotify::SDK
+      end
     end
+  end
+
+  class Errors
+    ##
+    # A Error class for when the payload is not a Hash instance.
+    #
+    class ModelPayloadExpectedToBeHashError < StandardError; end
+
+    ##
+    # A Error class for when the parent is not a Spotify::SDK instance.
+    #
+    class ModelParentInvalidSDKObjectError < StandardError; end
   end
 end
---
+++ spec/lib/spotify/sdk/model_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Spotify::SDK::Model do
+  let(:payload) { {id: 123_456, username: "example", country_code: "GB"} }
+  let(:sdk) { Spotify::SDK.new("access_token") }
+
+  context "good initialization" do
+    it "should not raise an error" do
+      expect {
+        Spotify::SDK::Model.new(payload, sdk)
+      }.not_to raise_error
+    end
+  end
+
+  context "bad initialization" do
+    context "parameters count" do
+      it "should raise an error if zero parameters are given" do
+        expect {
+          Spotify::SDK::Model.new
+        }.to raise_error ArgumentError
+      end
+
+      it "should raise an error if only one parameter is given" do
+        expect {
+          Spotify::SDK::Model.new(payload)
+        }.to raise_error ArgumentError
+      end
+    end
+
+    context "first parameter" do
+      it "should raise an error if not a hash" do
+        expect {
+          Spotify::SDK::Model.new("Hi world", sdk)
+        }.to raise_error Spotify::Errors::ModelPayloadExpectedToBeHashError
+      end
+    end
+
+    context "second parameter" do
+      it "should raise an error if not a hash" do
+        expect {
+          Spotify::SDK::Model.new(payload, "Hi world")
+        }.to raise_error Spotify::Errors::ModelParentInvalidSDKObjectError
+      end
+    end
+  end
+end
