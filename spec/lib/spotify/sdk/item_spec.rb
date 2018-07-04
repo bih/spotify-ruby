e8b49d317db0197b501f90202581246fb35e60a7
Bilawal Hameed <bil@spotify.com>
2018-07-04 23:48:37 +0200

    Added all primary Item helper methods

--- spec/lib/spotify/sdk/item_spec.rb
+++ spec/lib/spotify/sdk/item_spec.rb
@@ -11,4 +11,52 @@ RSpec.describe Spotify::SDK::Item do
       expect(subject.to_h).to eq raw_data
     end
   end
+
+  describe "#duration" do
+    it "is an alias for #duration_ms" do
+      expect(subject.duration).to eq subject.duration_ms
+    end
+  end
+
+  describe "#explicit?" do
+    it "is an alias for #explicit" do
+      expect(subject.explicit?).to eq subject.explicit
+    end
+  end
+
+  describe "#local?" do
+    it "is an alias for #is_local" do
+      expect(subject.local?).to eq subject.is_local
+    end
+  end
+
+  describe "#playback?" do
+    it "is an alias for #is_playback" do
+      expect(subject.playback?).to eq subject.is_playback
+    end
+  end
+
+  describe "#track?" do
+    it "returns false if #type is not a track" do
+      subject.type = "not_a_track"
+      expect(subject.track?).to eq false
+    end
+
+    it "returns true if #type is a track" do
+      subject.type = "track"
+      expect(subject.track?).to eq true
+    end
+  end
+
+  describe "#spotify_uri" do
+    it "is an alias for #uri" do
+      expect(subject.spotify_uri).to eq subject.uri
+    end
+  end
+
+  describe "#spotify_url" do
+    it "is an alias for #external_urls.spotify" do
+      expect(subject.spotify_url).to eq subject.external_urls[:spotify]
+    end
+  end
 end
