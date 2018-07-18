2f925e8fe43631c644a2a1bf4a4bae7013e95e34
Bilawal Hameed <bil@spotify.com>
2018-07-19 00:33:00 +0200

    setup basic tests for me/info

--- spec/lib/spotify/sdk/me/info_spec.rb
+++ spec/lib/spotify/sdk/me/info_spec.rb
@@ -3,12 +3,14 @@
 require "spec_helper"
 
 RSpec.describe Spotify::SDK::Me::Info do
-  # let(:raw_data) { read_fixture("get/v1/info/object") }
-  # subject { build(:info, raw_data) }
+  let(:raw_data) { read_fixture("get/v1/me/object") }
+  let(:session) { build(:session, access_token: "access_token") }
+  let(:me_sdk) { Spotify::SDK::Me.new(Spotify::SDK.new(session)) }
+  subject { Spotify::SDK::Me::Info.new(raw_data, me_sdk) }
 
-  # describe "#to_h" do
-  #   it "returns the correct value" do
-  #     expect(subject.to_h).to eq raw_data
-  #   end
-  # end
+  describe "#to_h" do
+    it "returns the correct value" do
+      expect(subject.to_h).to eq raw_data
+    end
+  end
 end
