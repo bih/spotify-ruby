2c24ac7b475a6f97a6a39bcce2e9471527875465
Bilawal Hameed <bil@spotify.com>
2018-07-20 23:41:17 +0200

    Implemented me.info method

--- lib/spotify/sdk/me.rb
+++ lib/spotify/sdk/me.rb
@@ -3,6 +3,23 @@
 module Spotify
   class SDK
     class Me < Base
+      ##
+      # Get the current user's information.
+      # GET /v1/me
+      #
+      # @example
+      #   me = @sdk.me.info
+      #
+      # @see https://developer.spotify.com/console/get-current-user/
+      # @see https://developer.spotify.com/documentation/web-api/reference/users-profile/get-current-users-profile/
+      #
+      # @param [Hash] override_opts Custom options for HTTParty.
+      # @return [Spotify::SDK::Me::Info] user_info Return the user's information.
+      #
+      def info(override_opts={})
+        me_info = send_http_request(:get, "/v1/me", override_opts)
+        Spotify::SDK::Me::Info.new(me_info, self)
+      end
     end
   end
 end
--- spec/lib/spotify/sdk/me_spec.rb
+++ spec/lib/spotify/sdk/me_spec.rb
@@ -3,6 +3,22 @@
 require "spec_helper"
 
 RSpec.describe Spotify::SDK::Me do
-  # let(:session) { build(:session, access_token: "access_token") }
-  # subject { Spotify::SDK.new(session).me }
+  let(:session) { build(:session, access_token: "access_token") }
+  subject { Spotify::SDK.new(session).me }
+
+  describe "#info" do
+    before(:each) do
+      stub_spotify_api_request(fixture:  "get/v1/me/object",
+                               method:   :get,
+                               endpoint: "/v1/me")
+    end
+
+    it "should return a Spotify::SDK::Me::Info object" do
+      expect(subject.info).to be_kind_of(Spotify::SDK::Me::Info)
+    end
+
+    it "should return the correct values" do
+      expect(subject.info.to_h).to eq read_fixture("get/v1/me/object")
+    end
+  end
 end
