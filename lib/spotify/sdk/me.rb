579bd42770add96bda2917acaa1c3dc38764c3c9
Bilawal Hameed <bil@spotify.com>
2018-07-21 12:06:10 +0200

    Added following method

--- lib/spotify/sdk/me.rb
+++ lib/spotify/sdk/me.rb
@@ -5,6 +5,7 @@ module Spotify
     class Me < Base
       ##
       # Get the current user's information.
+      # Respective information requires the `user-read-private user-read-email user-read-birthdate` scopes.
       # GET /v1/me
       #
       # @example
@@ -19,6 +20,32 @@ module Spotify
       def info(override_opts={})
         me_info = send_http_request(:get, "/v1/me", override_opts)
         Spotify::SDK::Me::Info.new(me_info, self)
+      end
+
+      ##
+      # Get the current user's followed artists. Requires the `user-read-follow` scope.
+      # GET /v1/me/following
+      #
+      # @example
+      #   @sdk.me.following
+      #
+      # @param [Hash] override_opts Custom options for HTTParty.
+      # @return [Array] artists A list of followed artists, wrapped in Spotify::SDK::Artist
+      #
+      def following(override_opts={})
+        artists = send_multiple_following_http_requests("/v1/me/following?type=artist&limit=50", override_opts)
+        artists.map do |artist|
+          Spotify::SDK::Artist.new(artist, self)
+        end
+      end
+
+      private
+
+      def send_multiple_following_http_requests(http_path, override_opts) # :nodoc:
+        request = send_http_request(:get, http_path, override_opts)[:artists]
+        artists = request[:items]
+        artists << send_multiple_following_http_requests(request[:next][23..-1], override_opts) if request[:next]
+        artists.flatten
       end
     end
   end
--- spec/lib/spotify/sdk/me_spec.rb
+++ spec/lib/spotify/sdk/me_spec.rb
@@ -21,4 +21,59 @@ RSpec.describe Spotify::SDK::Me do
       expect(subject.info.to_h).to eq read_fixture("get/v1/me/object")
     end
   end
+
+  describe "#following" do
+    it "should make one request if first request is empty" do
+      stub = stub_spotify_api_request(fixture:  "get/v1/me/following/empty-response",
+                                      method:   :get,
+                                      endpoint: "/v1/me/following?type=artist&limit=50")
+
+      subject.following
+      expect(stub).to have_been_requested.times(1)
+    end
+
+    it "should make one request if first request next is null" do
+      stub = stub_spotify_api_request(fixture:  "get/v1/me/following/valid-response-without-next",
+                                      method:   :get,
+                                      endpoint: "/v1/me/following?type=artist&limit=50")
+
+      subject.following
+      expect(stub).to have_been_requested.times(1)
+    end
+
+    it "should make two requests if second request next is null" do
+      first_stub = stub_spotify_api_request(fixture:  "get/v1/me/following/valid-response-with-next",
+                                            method:   :get,
+                                            endpoint: "/v1/me/following?type=artist&limit=50")
+
+      second_stub = stub_spotify_api_request(fixture:  "get/v1/me/following/valid-response-without-next",
+                                             method:   :get,
+                                             endpoint: "/v1/me/following?after=1uNFoZAHBGtllmzznpCI3s&limit=50&type=artist")
+
+      subject.following
+      expect(first_stub).to have_been_requested.times(1)
+      expect(second_stub).to have_been_requested.times(1)
+    end
+
+    it "should return N artists if valid response returns N artists" do
+      stub_spotify_api_request(fixture:  "get/v1/me/following/valid-response-with-next",
+                               method:   :get,
+                               endpoint: "/v1/me/following?type=artist&limit=50")
+
+      stub_spotify_api_request(fixture:  "get/v1/me/following/valid-response-without-next",
+                               method:   :get,
+                               endpoint: "/v1/me/following?after=1uNFoZAHBGtllmzznpCI3s&limit=50&type=artist")
+
+      expect(subject.following).to be_kind_of(Array).and have(18).items
+      expect(subject.following[0]).to be_kind_of(Spotify::SDK::Artist)
+    end
+    #
+    it "should return an empty array if empty response" do
+      stub_spotify_api_request(fixture:  "get/v1/me/following/empty-response",
+                               method:   :get,
+                               endpoint: "/v1/me/following?type=artist&limit=50")
+
+      expect(subject.following).to be_kind_of(Array).and have(0).items
+    end
+  end
 end
